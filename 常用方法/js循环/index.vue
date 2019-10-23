a08f58925a7bcf07d9a64d3f46581afff6aa2094
water <wt215484587@qq.com>
2019-10-23 17:36:10 +0800

    js方法

---
+++ 常用方法/js循环/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>js 循环</h2>
+
+        <p>for in </p>
+        <p>for of </p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+            var arr = ['a','s','f','g','h']
+            // for in 
+            for (const key in arr) {
+                console.log(key)  // 0 1 2 3 4 
+            }
+            // for of
+            for (const i of arr) {
+                console.log(i)    // a s f g h
+            }
+    },
+    methods: {
+        
+
+
+
+
+
+    },
+}
+</script>
